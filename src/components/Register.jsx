55bf2f6c68cc0b3b4b43e7bbfc109454aaa33f7f
yo20471214 <yogeshk24@apple.com>
2026-02-27 02:44:50 -0800

    Add home link to LegalIQ logo and make all text white on register page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -209,7 +209,7 @@ const Register = ({ onRegister }) => {
     <div className="register-container">
       <div className="register-left">
         <div className="register-branding">
-          <div className="logo-section">
+          <Link to="/" className="logo-section">
             <div className="logo-icon">
               <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 2L3 7V11C3 16.55 6.84 21.74 12 23C17.16 21.74 21 16.55 21 11V7L12 2Z" fill="currentColor"/>
@@ -217,7 +217,7 @@ const Register = ({ onRegister }) => {
               </svg>
             </div>
             <h1 className="logo-text">LegalIQ</h1>
-          </div>
+          </Link>
           <h2 className="tagline">Join India's Leading Legal Platform</h2>
           <p className="description">
             {userType === 'lawyer' 
